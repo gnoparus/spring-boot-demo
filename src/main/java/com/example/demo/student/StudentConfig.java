f9b4d4f678132771324444c4f8e568e2da7fdcee
gnoparus <surapong@gmail.com>
2023-02-04 20:31:35 +0700

    Populate data

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+    
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student amy= new Student("Amy", "amy11@gmail.com", LocalDate.of(1981, Month.JANUARY, 1), 11);
+            Student bobby = new Student("Bobby", "bobby22@gmail.com", LocalDate.of(1982, Month.FEBRUARY, 2), 12);
+
+            repository.saveAll(List.of(amy, bobby));
+        };
+    }
+}
